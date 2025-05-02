c61b9f907531e065e52e4f1836eee47352727442
Lita0303 <litamile500@gmail.com>
2025-05-02 19:10:08 +0400

    Python CHAPTER 1-5

---
+++ CHAPTER2/chapter_02.py
@@ -0,0 +1,12 @@
+#print("Hello World")
+
+#message = "Hello Python World!"
+#print("\nmessage")
+
+#myName = "Litamile"
+#mySurname = "Vimbi"
+#myFullName = f"{myName} {mySurname}"
+#print(myFul
+
+0.1 
+2 * 0.1
---
+++ CHAPTER4/chapter_04.py
@@ -0,0 +1,15 @@
+#create an array and make a for loop
+my_list = ["apples", "oranges", "grapes", "peaches", "strawberries"]
+for fruit in my_list:
+    print(f"I am craving {fruit}, how much does it cost?")
+print("I'll have all of them please.")
+
+
+
+
+# range(): from 10 to 20 count in 2's
+for each_number in range(10, 20, 2 ):
+    print(each_number)
+
+my_new_list = list(range(0,100, 5))
+print(my_new_list)
